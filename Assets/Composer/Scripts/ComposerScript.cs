e4080d15653b8645ed8ecee9d3c478159bc512a4
PoorMansTurbo <nickdutka@gmail.com>
2023-01-26 15:59:26 -0800

    Composer Complete

--- Assets/Composer/Scripts/ComposerScript.cs
+++ Assets/Composer/Scripts/ComposerScript.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class ComposerScript : MonoBehaviour
 
@@ -20,15 +22,13 @@ public class ComposerScript : MonoBehaviour
                        f4Sharp, 
                        g4, 
                        g4Sharp;
-                       
-    private List<AudioSource> notesInComposition = new List<AudioSource>();
+
+    public TMP_Text compositionText;
+               
+    public List<AudioSource> notesInComposition = new List<AudioSource>();
 
     private IEnumerator PlaySong()
     {
-        //notesInComposition.Add(a4);
-        //notesInComposition.Add(a4);
-        //notesInComposition.Add(a4Sharp);
-        //notesInComposition.Add(a4);
         
         for (int i = 0; i < notesInComposition.Count; i++)
         {
@@ -44,83 +44,49 @@ public class ComposerScript : MonoBehaviour
         }
     }
 
-    public void Adda4NoteToSong()
+    public void AddNoteToSong(AudioSource note)
     {
-        notesInComposition.Add(a4);
-        Debug.Log(a4);
-    }
-    public void Addasharp4NoteToSong()
-    {
-        notesInComposition.Add(a4Sharp);
-        Debug.Log(a4Sharp);
+        notesInComposition.Add(note);
+
+        string output = "";
+
+        for (int i = 0; i < notesInComposition.Count; i++)
+        {
+            output += notesInComposition[i].name;
+            output += " ";
+        }
+
+        compositionText.text = output;
+        
+        //foreach (var item in notesInComposition)
+        //{
+        //    Debug.Log(item.name);
+        //}
     }
 
+    //public void AddNoteName(string notename)
+    //{
+    //    compositionText.text = compositionText.text + " " + notename;
+    //}
+
+    public void RemoveNoteFromSong()
+    {
+        notesInComposition.Remove(notesInComposition[notesInComposition.Count - 1]);
+
+        string output = "";
+
+        for (int i = 0; i < notesInComposition.Count; i++)
+        {
+            output += notesInComposition[i].name;
+            output += " ";
+        }
+
+        compositionText.text = output;
+    }
+    
     public void StartSong()
     {
         StartCoroutine(PlaySong());
     }
 
-    void Update()
-    {
-        if (Keyboard.current.aKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(a4);
-            a4.Play();
-        }
-        if (Keyboard.current.wKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(a4Sharp);
-            a4Sharp.Play();
-        }
-        if (Keyboard.current.sKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(b4);
-            b4.Play();
-        }
-        if (Keyboard.current.dKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(c4);
-            c4.Play();
-        }
-        if (Keyboard.current.rKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(c4Sharp);
-            c4Sharp.Play();
-        }
-        if (Keyboard.current.fKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(d4);
-            d4.Play();
-        }
-        if (Keyboard.current.tKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(d4Sharp);
-            d4Sharp.Play();
-        }
-        if (Keyboard.current.gKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(e4);
-            e4.Play();
-        }
-        if (Keyboard.current.hKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(f4);
-            f4.Play();
-        }
-        if (Keyboard.current.uKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(f4Sharp);
-            f4Sharp.Play();
-        }
-        if (Keyboard.current.jKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(g4);
-            g4.Play();
-        }
-        if (Keyboard.current.iKey.wasPressedThisFrame)
-        {
-            notesInComposition.Add(g4Sharp);
-            g4Sharp.Play();
-        }
-    }
 }
