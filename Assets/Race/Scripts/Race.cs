00d8956da64a30acfce9ea07ce941ee71c47b60c
PoorMansTurbo <nickdutka@gmail.com>
2023-01-26 16:48:36 -0800

    Composer cleanup + Race scene started.

---
+++ Assets/Race/Scripts/Race.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class Race : MonoBehaviour
+{
+ 
+
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
