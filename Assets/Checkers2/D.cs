ea149130effb2aa2787424aa15ae8374ba746eea
NickDutka <nickdutka@gmail.com>
2023-03-06 12:10:02 -0800

    Both Checkers soloutions finished.
    worked on the second solution and finished both.

--- Assets/Checkers2/D.cs
+++ Assets/Checkers2/D.cs
@@ -3,13 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class D : MonoBehaviour
 {
     static string k, k2 = "";
     static Image C;
     static Color color = Color.white;
     public GameObject P;
+    public TMP_Text r, w;
+
+
 
     public void Cclick(RectTransform t)
     {
@@ -56,24 +59,6 @@ public class D : MonoBehaviour
         }
     }
 
-    public Text r, w;
-
-    public void A2(char c)
-    {
-        if (c == 'w') S.Cmp.x++;
-        else S.Cmp.y++;
-        if(S.Cmp.x >= 12)
-        {
-            P.gameObject.SetActive(true);
-            P.transform.Find("w").GetComponent<Text>().text = "You win white";
-
-        }
-        if (S.Cmp.y >= 12)
-        {
-            P.gameObject.SetActive(true);
-            P.transform.Find("w").GetComponent<Text>().text = "You win red";
-        }
-    }
 
     public void Kclick(RectTransform t)
     {
@@ -83,8 +68,41 @@ public class D : MonoBehaviour
         i = Convert.ToInt32((name.Split('&'))[0]);
         j = Convert.ToInt32((name.Split('&'))[1]);
         S.grid[i, j].transform.Find("C").GetComponent<Image>().color = color;
+        S.grid[i, j].transform.Find("C").GetComponent<Image>().enabled = true;
+        i = Convert.ToInt32((k.Split(' '))[0]);
+        j = Convert.ToInt32((k.Split(' '))[1]);
+        S.grid[i, j].transform.Find("C").GetComponent<Image>().enabled = false;
+        if (k2 != "" && ((Convert.ToInt32((name.Split('&'))[1]) - 1) == Convert.ToInt32((k2.Split(' '))[1]) || (Convert.ToInt32((name.Split('&'))[1] + 1) == Convert.ToInt32((k2.Split(' '))[1]))))
+        {
+            i = Convert.ToInt32((k2.Split(' '))[0]);
+            j = Convert.ToInt32((k2.Split(' '))[1]);
+            S.grid[i, j].transform.Find("C").GetComponent<Image>().enabled = false;
+            if (color == Color.white) A2('w');
+            else A2('r');
+            r.text = S.Cmp.x + "";
+            w.text = S.Cmp.y + "";
 
+            k2 = "";
+        }
+        if (color == Color.white) color = Color.red;
+        else color = Color.white;
+    }
 
+    public void A2(char c)
+    {
+        if (c == 'w') S.Cmp.x++;
+        else S.Cmp.y++;
+        if (S.Cmp.x >= 12)
+        {
+            P.gameObject.SetActive(true);
+            P.transform.Find("w").GetComponent<TMP_Text>().text = "You win white";
+
+        }
+        if (S.Cmp.y >= 12)
+        {
+            P.gameObject.SetActive(true);
+            P.transform.Find("w").GetComponent<TMP_Text>().text = "You win red";
+        }
     }
 
 }
