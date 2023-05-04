2ae1a4151c0099d7042d5c0ce250111c25930953
PoorMansTurbo <nickdutka@gmail.com>
2023-05-04 16:12:40 -0700

    Added all AR stuff | pushing test project to test at home

--- Assets/Bolf/Scripts/MainMenu.cs
+++ Assets/Bolf/Scripts/MainMenu.cs
@@ -19,7 +19,7 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
+        //UnityEditor.EditorApplication.isPlaying = false;
         //Application.Quit();
     }
 }
